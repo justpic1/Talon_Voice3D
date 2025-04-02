0b853653ba44792c0d8fa8af6e41fb7be32b1c82
Cyberneticbacon <Cyberneticbacon@gmail.com>
2025-04-02 09:28:47 -0500

    Decimal Numbers

--- length.py
+++ length.py
@@ -3,7 +3,7 @@ from talon import Module, Context
 mod = Module()
 ctx = Context()
 
-@mod.capture(rule="<user.number_string> [{user.unit}]")
+@mod.capture(rule="<user.number_prose_with_dot> [{user.unit}]")
 def length(m) -> str:
     print("length")
     return m
